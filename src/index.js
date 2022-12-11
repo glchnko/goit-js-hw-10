beac799fef29a3bb455ce2fb1f04179787721752
Anna <agalucnko@gmail.com>
2022-12-11 23:43:47 +0200

    add changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries.js';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
